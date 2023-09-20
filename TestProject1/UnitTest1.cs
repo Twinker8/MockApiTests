8ff3c94654e8efd56d9bf7f149fb5480732fc214
Do Min Tkhan Lonh <Do_Min_Tkhan_Lonh@epam.com>
2023-09-20 12:57:48 +0200

    added new comment

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace TestProject1
 {
     public class Tests
     {
-        //adf
+        //NewComments
         private static readonly TestHttpClient<UserDatacs> _users = new TestHttpClient<UserDatacs>();
         private readonly HttpClient _client= new HttpClient();
         private const string Url = "https://6507126a3a38daf4803f13f0.mockapi.io/user/";
